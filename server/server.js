5a50a3dded6f46467c73f696b428716dac7293d4
Your Name <you@example.com>
2022-03-15 18:05:50 +0200

    mysql link ok

--- server/server.js
+++ server/server.js
@@ -1,3 +1,21 @@
-import mysql from 'mysql';
+import mysql from 'mysql2';
 
-const connection 
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password:'Aa123456!',
+    database: 'cms'
+})
+
+connection.query(
+    'Select * from customers',
+    (error,results)=> {
+       if (error){
+           console.log(error)
+       } else {
+           console.log(results)
+       }
+    }
+)
+
+export default connection;
